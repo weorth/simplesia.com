8764084e8e72a9b9512ce3b17ae79f30add69510
g-weorth <giovana@weorth.ai>
2023-08-07 15:18:31 -0700

    update content and translate it to en-us

--- src/pages/PublicHome.vue
+++ src/pages/PublicHome.vue
@@ -19,12 +19,12 @@ function handleRegister() {
       <div class="slogan">
         <language-panel code="en_us">
           <h1>The intelligent APIs for your business</h1>
-          <h2>We take care of the Artificial Intelligence tools while you focus your intelligent process</h2>
+          <h2>We take care of the Artificial Intelligence tools while you focus on your products </h2>
           <h3>Ready to go or upon demand!</h3>
         </language-panel>
         <language-panel code="pt_br">
           <h1>As APIs inteligentes do seu negócio</h1>
-          <h2>Nós cuidamos das ferramentas de Inteligência Artificial enquanto você foca nos seus processos inteligentes</h2>
+          <h2>Nós cuidamos das ferramentas de Inteligência Artificial enquanto você foca nos seus produtos</h2>
           <h3>A pronta entrega ou sob demanda! Nesta versão beta nós estamos oferecendo as APIs públicas da plataforma Simplesia para testes sem o custo de mensalidade. Peça a sua chave de acesso!</h3>
         </language-panel>
         <div class="actions">
@@ -47,20 +47,20 @@ function handleRegister() {
           <p>We are responsible to keep the model accuracy according to the natural process changes (for example, new cars on the market)</p>
         </div>
         <div class="feature">
-          <h3>Aliquam bibendum lectus</h3>
-          <p>Maecenas quam risus, posuere ullamcorper mi vel, semper luctus odio. Curabitur nec iaculis odio.</p>
+          <h3>No vendor lock-in</h3>
+          <p>You pay for what you use and once it does not make sense anymore, you are free to go.</p>
         </div>
         <div class="feature">
-          <h3>Aliquam bibendum lectus</h3>
-          <p>Maecenas quam risus, posuere ullamcorper mi vel, semper luctus odio. Curabitur nec iaculis odio.</p>
+          <h3>Couldn't find what you need?</h3>
+          <p>Our team is ready to work with you. Order your customized model with the possibility of getting it for free.</p>
         </div>
         <div class="feature">
-          <h3>Aliquam bibendum lectus</h3>
-          <p>Maecenas quam risus, posuere ullamcorper mi vel, semper luctus odio. Curabitur nec iaculis odio.</p>
+          <h3>Easy pay and manage your account</h3>
+          <p>Follow up your monthly invoices and requests inside our platform for users.</p>
         </div>
         <div class="feature">
-          <h3>Aliquam bibendum lectus</h3>
-          <p>Maecenas quam risus, posuere ullamcorper mi vel, semper luctus odio. Curabitur nec iaculis odio.</p>
+          <h3>Do you need an exclusive product?</h3>
+          <p>Is more privacy required? We also develop APIs for your own usage.</p>
         </div>
       </language-panel>
       <language-panel code="pt_br">
@@ -73,16 +73,16 @@ function handleRegister() {
           <p>Nós nos responsabilizamos de manter a acurácia do modelo de acordo com as mudanças naturais de processo (por exemplo, novos carros no mercado).</p>
         </div>
         <div class="feature">
-          <h3>Sem prazo de fidelidade</h3>
+          <h3>Sem fidelização</h3>
           <p>Você paga pelo que usa e se não fizer mais sentido, você está livre para novos rumos.</p>
         </div>
         <div class="feature">
-          <h3>Não encontrou o que precisava?</h3>
+          <h3>Não encontrou o que precisa?</h3>
           <p>Nosso time está pronto pra te atender. Peça o seu modelo customizado com a possibilidade de desenvolvimento completamente gratuito.</p>
         </div>
         <div class="feature">
-          <h3>Facilidade no gerenciamento e pagamento</h3>
-          <p>Acompanhe as suas requisições e cobranças mensais dentro da nossa plataforma de gestão para os usuários.</p>
+          <h3>Facilidade no pagamento e gerenciamento da sua conta</h3>
+          <p>Acompanhe as suas requisições e cobranças mensais dentro da nossa plataforma para os usuários.</p>
         </div>
         <div class="feature">
           <h3>Precisa de um produto exclusivo?</h3>
@@ -93,53 +93,53 @@ function handleRegister() {
     <div class="testimonials">
       <language-panel code="en_us">
         <div class="testimonial">
-          <blockquote>Proin tincidunt ut lectus sit amet gravida. Integer maximus turpis eget purus posuere finibus.</blockquote>
-          <h5>By John Doe</h5>
+          <blockquote>How can I access the APIs?</blockquote>
+          <h5>The APIs are divided into seven major groups: Animal, Food, Human, Medical, Plant, Security, and Vehicles. These groups were selected among the most basic ones for initial development in our platform. To use our APIs you need to register and access the key for requests. In this beta version the keys are offered under demand. Ask yours!</h5>
         </div>
         <div class="testimonial">
-          <blockquote>Proin tincidunt ut lectus sit amet gravida. Integer maximus turpis eget purus posuere finibus.</blockquote>
-          <h5>By John Doe</h5>
+          <blockquote>What is the cost to use the platform and APIs?</blockquote>
+          <h5>The price of the platform is U$x.00 per month. This value is paid only when you create the key to access the APIs. Besides the monthly costs, you pay the number of requests in each API (see our Product page to know the values). The monthly extract for payments and requests can be seen in your profile.</h5>
         </div>
         <div class="testimonial">
-          <blockquote>Proin tincidunt ut lectus sit amet gravida. Integer maximus turpis eget purus posuere finibus.</blockquote>
-          <h5>By John Doe</h5>
+          <blockquote>Why do I have to pay a monthly fee to use the platform?</blockquote>
+          <h5>In order to offer a better experience to all of our customers, we need to keep software environments and managing tools working and ready to automatically scale according to your process. We are transparent and we do not want to hide abusive profits, but offer quality and agility to you.</h5>
         </div>
         <div class="testimonial">
-          <blockquote>Proin tincidunt ut lectus sit amet gravida. Integer maximus turpis eget purus posuere finibus.</blockquote>
-          <h5>By John Doe</h5>
+          <blockquote>What if the API request does not work?</blockquote>
+          <h5>Our system detects all errors caused internally and does not add count on your amount of total requests.</h5>
         </div>
         <div class="testimonial">
-          <blockquote>Proin tincidunt ut lectus sit amet gravida. Integer maximus turpis eget purus posuere finibus.</blockquote>
-          <h5>By John Doe</h5>
+          <blockquote>How can I get my customized model without any development costs?</blockquote>
+          <h5>All of customization requests are assessed by our team before the final pricing. Models already under development or models of simple conceptions might be for free to our clients.</h5>
         </div>
         <div class="testimonial">
-          <blockquote>Proin tincidunt ut lectus sit amet gravida. Integer maximus turpis eget purus posuere finibus.</blockquote>
-          <h5>By John Doe</h5>
+          <blockquote>Is it safe to add my credit card on my account?</blockquote>
+          <h5>We do not keep your credit card information on our database. Moreover, we have all of the certificates that proof the identity and integrity of the company.</h5>
         </div>
       </language-panel>
       <language-panel code="pt_br">
         <div class="testimonial">
-          <blockquote>Como funcionam as APIs? Como posso ter acesso?</blockquote>
-          <h5>As APIs são divididas e grandes 7 grupos: Animal, Comida, Humano, Medicina, Planta, Segurança e Veículos. Estes grupos foram selecionados entre os mais básicos para desenvolvimento inicial da nossa plataforma. Para utilizá-las você precisa ter cadastro e acesso à chave de requisições. Nesta versão beta as chaves são oferecidas sob demanda. Peça a sua chave de acesso!</h5>
+          <blockquote>Como eu posso ter acesso às APIs?</blockquote>
+          <h5>As APIs são divididas em sete grandes grupos: Animal, Comida, Humano, Medicina, Planta, Segurança e Veículos. Estes grupos foram selecionados entre os mais básicos para desenvolvimento inicial da nossa plataforma. Para utilizá-las você precisa ter cadastro e acesso à chave de requisições. Nesta versão beta as chaves são oferecidas sob demanda. Peça a sua chave de acesso!</h5>
         </div>
         <div class="testimonial">
-          <blockquote>Qual é o custo de uso da plataforma e API?</blockquote>
-          <h5>O custo da plataforma é de R$ x,00 mensais. Esse valor é pago apenas quando você cria a chave para acesso nas APIs. Além do custo mensal, você paga por requisições feitas por APIs (veja nossa página de Produtos para saber os valores de cada uma). O extrato mensal de pagamentos e requisições podem ser vistos no perfil.</h5>
+          <blockquote>Qual é o custo de uso da plataforma e APIs?</blockquote>
+          <h5>O custo da plataforma é de R$ x,00 mensais. Esse valor é pago apenas quando você cria a chave para acesso nas APIs. Além do custo mensal, você paga por requisições feitas por APIs (veja nossa página de Produtos para saber os valores de cada uma). O extrato mensal de pagamentos e requisições podem ser vistos no seu perfil.</h5>
         </div>
         <div class="testimonial">
           <blockquote>Por que preciso pagar uma mensalidade para usar a plataforma?</blockquote>
-          <h5>Para oferecermos uma melhor experiência de uso para os nossos clientes, precisamos manter ambientes de software e ferramentas de suporte funcionando e prontos para escalabilidade do seus processos. Um dos nossos valores é transparência e aqui não queremos esconder rentabilidade abusivas. Nosso foco é oferecer qualidade e agilidade para os seus processos.</h5>
+          <h5>Para oferecermos uma melhor experiência de uso para os nossos clientes, precisamos manter ambientes de software e ferramentas de suporte funcionando e prontos para escalabilidade automática dos seus processos. Nós não queremos esconder rentabilidade abusivas, apenas focamos em oferecer qualidade e agilidade para você.</h5>
         </div>
         <div class="testimonial">
-          <blockquote>E se a API não funcionar?</blockquote>
-          <h5>Nosso sistema detecta todos os erros internos das nossas APIs e, quando isso ocorre, não há acréscimos nas contagens de requisições.</h5>
+          <blockquote>E se a requisição da API não funcionar?</blockquote>
+          <h5>Nosso sistema detecta todos os erros causados internamente e não acrescenta contagem nas requisições totais.</h5>
         </div>
         <div class="testimonial">
           <blockquote>Como posso obter meu modelo customizado sem custos de desenvolvimento?</blockquote>
-          <h5>Todas as requisições de customização de modelos são análisadas pelos nossos times antes de qualquer precificação final. Modelos já em desenvolvimento pelo time ou de simples concepção podem sair sem custos para os nossos clientes.</h5>
+          <h5>Todas as requisições de customização de modelos são análisadas pelos nossos times antes de qualquer precificação final. Modelos já em desenvolvimento ou de simples concepção podem sair sem custos para os nossos clientes.</h5>
         </div>
         <div class="testimonial">
-          <blockquote>Como sei que a plataforma é segura para adicionar o meu cartão de crédito?</blockquote>
+          <blockquote>É seguro adicionar um cartão de crédito na minha conta?</blockquote>
           <h5>Nós não mantemos a sua informação de cartão de crédito salvas em nosso banco de dados. Além disso, temos as certificações de segurança que comprovam a identidade e integridade da empresa.</h5>
         </div>
       </language-panel>
